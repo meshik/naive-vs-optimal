c5985c5e53dcc57771f983a2d5dd79ccfe68d483
Dekel Wainer <58933658+meshik@users.noreply.github.com>
2024-01-16 21:13:47 +0100

    final version, off to scripting

--- remove_dup_sorted_array/main.py
+++ remove_dup_sorted_array/main.py
@@ -4,7 +4,6 @@ def naïve_approach_v1(nums: list):
 
     # iterate through the array
     for num in nums:
-        # check if 'num' is not in unique_nums
         if num not in unique_nums:
             # add 'num' to unique_nums
             unique_nums.append(num)
@@ -12,7 +11,7 @@ def naïve_approach_v1(nums: list):
     # set nums to unique_nums
     nums = unique_nums
     # return number of unique elements
-    return len(unique_nums)
+    return len(nums)
 # time complexity: O(n^2) - loop + 'in' operator
 # space complexity: O(n) -  unique_nums list
 
@@ -20,16 +19,18 @@ def naïve_approach_v1(nums: list):
 def naïve_approach_v2(nums: list):
     # initialize index
     index = 0
+    
     # loop through the whole array
     while index < len(nums) - 1:
-        # check if current and next elements are the same
+        
         if nums[index] == nums[index + 1]:
-            # remove the next element
+            # remove the duplicate
             nums.pop(index + 1)
         else:
-            # Move to the next element
+            # keep advancing
             index += 1
-    # Return the new length of nums
+            
+    # return final length of 'nums'
     return len(nums)
 # time complexity: O(n^2) - loop + 'pop' method
 # space complexity: O(1) - perfect
@@ -44,14 +45,17 @@ def optïmal_solution(nums: list):
 
     # iterate through the array
     while advancing_p < len(nums):
-        # check if the pointed elements are the same
+        
+        # check if pointed elements are the same
         if nums[unique_p] != nums[advancing_p]:
-            nums[unique_p + 1] = nums[advancing_p]
+            # assign new element as the next unique
             unique_p += 1
+            nums[unique_p] = nums[advancing_p]
+            
         advancing_p += 1
 
     # +1 because of 0 indexing
     return unique_p + 1
-
 # time complexity: O(n) - loop
 # space complexity: O(1) - perfect
+# life complexity: OH(YEA!)
